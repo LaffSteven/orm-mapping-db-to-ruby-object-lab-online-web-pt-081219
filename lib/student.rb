25ed73cf2dd6f35a6ebd7293909790b85cce9f32
Steven Laff <steve.laff@gmail.com>
2019-10-31 22:10:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -92,9 +92,8 @@ class Student
   end
   
   def self.first_X_students_in_grade_10(number)
-    student_list = []
     x = number.to_i
-    sql = <<~ SQL
+    sql = <<~SQL
       SELECT * FROM students WHERE grade = 10 LIMIT ?
     SQL
     
